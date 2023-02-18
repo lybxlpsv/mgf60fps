45921da2baf498347c857361be60a6f14301a6da
lybxlpsv <lybxlpsv@yahoo.com>
2023-02-18 10:33:29 +0700

    add PCSE00326 and fix path for linux usage

--- patch.ps1
+++ patch.ps1
@@ -6,9 +6,7 @@ if ( -Not(Test-Path "eboot.bin")) {
     Return
 }
 
-.\bin\vita-unmake-fself.exe eboot.bin
-$FileHash = Get-FileHash eboot.bin.elf -Algorithm MD5
-$bytes = [System.IO.File]::ReadAllBytes("eboot.bin.elf")
+$FileHash = Get-FileHash eboot.bin -Algorithm MD5
 
 if ( -Not(Test-Path "output")) {
     New-Item "output" -ItemType Directory
@@ -16,27 +14,48 @@ if ( -Not(Test-Path "output")) {
 
 [bool] $patched = 0
 
-# MGF Patch based on Diva X 60fps patch by someone idk who
-if ($FileHash.Hash -eq "BAF63B678E090A47F4C0CEA0478856E9") {
+# Diva X 60fps patch by someone idk who
+if ($FileHash.Hash -eq "733033E2DDF86D94FCA30B2AA1249302")
+{
+    $bytes = [System.IO.File]::ReadAllBytes("eboot.bin")
+    Write-Output "Patching 60fps Diva X ASIA"
+    $bytes[0x3A32C] = 0x1
+    #0x3932c
+    $bytes[0x3AA5A] = 0xf7
+    #0x39a5a
+    [System.IO.File]::WriteAllBytes("output/eboot.bin", $bytes)
+    $patched = 1
+}
+
+if ($FileHash.Hash -eq "9E50BD28879FC721AB724E97141F9D8A") {
+    $bytes = [System.IO.File]::ReadAllBytes("eboot.bin")
     Write-Output "Patching 60fps Miracle Girls Festival"
-    $bytes[0x4e464] = 0x1
-    $bytes[0x4e5c0] = 0xf7
+    $bytes[0x4F464] = 0x1
+    #0x4e464
+    $bytes[0x4F5C0] = 0xf7
+    #0x4e5c0
+    [System.IO.File]::WriteAllBytes("output/eboot.bin", $bytes)
+    $patched = 1
+}
+
+if ($FileHash.Hash -eq "3727CEE0C28313B961634C15B3F7EA33") {
+    $bytes = [System.IO.File]::ReadAllBytes("eboot.bin")
+    Write-Output "Patching 60fps Diva F US"
+    $bytes[0x4F767E] = 0x1
+    #0x4f667e
+    $bytes[0x14764] = 0x1
+    #0x13764
+    $bytes[0x149E8] = 0xf7
+    #0x139e8
+    [System.IO.File]::WriteAllBytes("output/eboot.bin", $bytes)
     $patched = 1
 }
 
 if ($patched) {
-    [System.IO.File]::WriteAllBytes("output\eboot.bin.elf", $bytes)
-
-    .\bin\vita-make-fself.exe output\eboot.bin.elf output\eboot.bin
-
-    Remove-Item eboot.bin.elf
-    Remove-Item output/eboot.bin.elf
-
     Write-Output "Done. output\eboot.bin"
 }
 else {
     Write-Output "Game is not supported."
-    Remove-Item eboot.bin.elf
 }
 
 Start-Sleep -Seconds 2
